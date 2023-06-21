fd7cc272177f4ea98812577f33daa6ae873d870e
Markku Leiniö <markkuleinio@users.noreply.github.com>
2023-06-21 19:44:56 +0300

    Fix data length in the header

--- zbxsend.py
+++ zbxsend.py
@@ -57,7 +57,7 @@ def send_to_zabbix(
         ensure_ascii=False,
         separators=(",", ":"),  # Remove spaces, to emulate zabbix_sender behaviour
     )
-    data_len = struct.pack('<Q', len(json_data))
+    data_len = struct.pack('<Q', len(json_data.encode()))
     packet = b'ZBXD\1' + data_len + json_data.encode()
     try:
         zabbix = socket.socket()
